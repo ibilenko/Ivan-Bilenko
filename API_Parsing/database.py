d2b35216808a2418aeb4d46b54b9c9cf75b8ea47
ibilenko <68779580+ibilenko@users.noreply.github.com>
2021-11-14 16:14:36 +0300

    Update database.py

--- API_Parsing/database.py
+++ API_Parsing/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://hmxdnwubdttbnz:9e63e8f51fbdaa5a7dfd36e69526ffd277c532dd124debf16f9ef3cebcea5d79@ec2-52-31-233-101.eu-west-1.compute.amazonaws.com:5432/d10gqjnnkpealj"
+SQLALCHEMY_DATABASE_URL = "postgresql://string"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
